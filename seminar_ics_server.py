20e15a5f077af307d68ee26999751b2de5efb00d
4iar <4iar@users.noreply.github.com>
2016-01-21 18:21:59 +0000

    Add text/calendar mimetype

--- seminar_ics_server.py
+++ seminar_ics_server.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask import Response
 from seminar_parser import fetch_events_as_ics
 
 app = Flask(__name__)
 
 @app.route('/ims_seminars/')
 def ims_seminars_as_ics():
-    return fetch_events_as_ics()
+    ics = fetch_events_as_ics()
+
+    return Response(ics, mimetype="text/calendar")
 
 if __name__ == '__main__':
     app.run()
