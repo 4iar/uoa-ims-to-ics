3cf112269467df93ceaf5a6c8283db1a067012f7
4iar <4iar@users.noreply.github.com>
2016-02-14 18:09:15 +0000

    Add redis connection

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import Response
-from seminar_parser import fetch_events_as_ics
+from flask.ext.redis import FlaskRedis
 
 app = Flask(__name__)
+
+redis_store = FlaskRedis(app)
 
 
 @app.route('/ims_seminars/')
 def ims_seminars_as_ics():
-    ics = fetch_events_as_ics()
+    ics = redis_store.get('calendar').decode('utf-8')
 
     return Response(ics, mimetype="text/calendar")
 
