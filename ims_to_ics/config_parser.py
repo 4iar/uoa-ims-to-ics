b821830d9312b4148652e2c72d52bc6f688bee23
4iar <4iar@users.noreply.github.com>
2016-03-22 20:07:52 +0000

    Organise modules into a subdirectory to keep root dir clean

--- ims_to_ics/config_parser.py
+++ ims_to_ics/config_parser.py
@@ -2,10 +2,9 @@ import configparser
 
 class Config():
 
-    def __init__(self):
-        config_filename = 'config.ini'
+    def __init__(self, config_filename='config.ini'):
         self.config = configparser.ConfigParser()
-        self.config.read(config_filename)
+        self.config.read('../{}'.format(config_filename))
 
     def getint(self, section, key):
 
--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import subprocess
-from update_calendar import update_calendar
+from ims_to_ics.update_calendar import update_calendar
 import time
-from config_parser import Config
+from ims_to_ics.config_parser import Config
 
 if __name__ == '__main__':
 
     config = Config()
 
-    subprocess.Popen(['python3', 'app.py'])
+    subprocess.Popen(['python3', './ims_to_ics/app.py'])
 
     while True:
         update_calendar()
