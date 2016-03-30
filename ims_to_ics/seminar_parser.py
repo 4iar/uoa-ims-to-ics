da9ab36d6b2955d890892a774d352b3adae052d3
4iar <4iar@users.noreply.github.com>
2016-03-30 18:24:47 +0100

    Refactor

--- ims_to_ics/seminar_parser.py
+++ ims_to_ics/seminar_parser.py
@@ -5,16 +5,26 @@ import requests
 EVENTS_RSS_URL = "https://www.abdn.ac.uk/ims/seminars/rss.xml"
 EVENTS_ICAL_URL = "https://www.abdn.ac.uk/ims/seminars/ical/{}/"
 
-def fetch_events_as_ics():
-    data = feedparser.parse(EVENTS_RSS_URL)
+
+def get_event_ids():
+    """
+    GET the events RSS and extract IDs for all events.
+    """
+    rss_page = requests.get(EVENTS_RSS_URL).text
+    data = feedparser.parse(rss_page)
 
     # Extract the unique event id from rss event entries
-    event_ids = [e['id'].split('/')[5] for e in data['entries']]
+    return [e['id'].split('/')[5] for e in data['entries']]
 
-    # Concatenate the events
+def fetch_events_as_ics():
+    event_ids = get_event_ids()
+
     calendar = "BEGIN:VCALENDAR\r\n"
     for event_id in event_ids:
-        event_ical = requests.get(EVENTS_ICAL_URL.format(event_id))
+        # GET the ics file using the unique event ID (from get_event_ids)
+        # then concat the ics files together
+
+        event_ical = requests.get(EVENTS_ICAL_URL.format(event_id)).text
         calendar += extract_event(event_ical)
 
     calendar += "END:VCALENDAR\r\n"
@@ -27,5 +37,5 @@ def extract_event(event_ical):
     so that we can concatenate the events
     """
 
-    return event_ical.text.replace("BEGIN:VCALENDAR\r\n", "")\
+    return event_ical.replace("BEGIN:VCALENDAR\r\n", "")\
         .replace("END:VCALENDAR\r\n", "")
